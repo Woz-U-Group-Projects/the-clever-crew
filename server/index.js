968f402c51c364e884939384a42e14313f56051b
Phillip Rose <rosecodingaccount@protonmail.com>
2021-07-31 23:43:12 -0700

    add connection to server

--- server/index.js
+++ server/index.js
@@ -2,3 +2,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "10mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+app.use(cors());
+
+const CONNECT_MONGODB_URL = 'mongodb+srv://dbuser:<password>@cluster0.frzgx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' //add username and password to database
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECT_MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => app.listen(PORT, () => console.log('DB connection worked')) )
+.catch((error) => console.log(error.message) );
+
+mongoose.set('useFindAndModify', false);
