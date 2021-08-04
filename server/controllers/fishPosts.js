cd4a9fb8ed37c1eada559b6527be04477c6a29f9
Phillip Rose <rosecodingaccount@protonmail.com>
2021-08-03 19:11:58 -0700

    added  posts and get  routes on server side and api folder and started with code for requests

--- server/controllers/fishPosts.js
+++ server/controllers/fishPosts.js
@@ -2,10 +2,24 @@ import PostInfo from '../models/postInfo.js'; // from model schema created
 
 
 
-export const getFishes = (req, res) => {
-    res.send('fish posts go here');
+export const getFishPosts =  async (req, res) => {
+    try {
+        const postMessages =  await PostInfo.find();
+        res.status(200).json(postMessages); //request has succeeded
+    } catch (error) {
+        res.status(404).json({ message: error.message }); //cannot find
+    }
 }
 
-export const createPost = (req, res) => {
-    res.send('this is a post')
+export const createPost = async (req, res) => {
+const post = req.body;
+
+const newPost = new PostInfo(post);
+
+    try {
+       await newPost.save();
+       res.status(201).json(newPost);  //Successfully created
+    } catch (error) {
+        res.status(409).json({ message: error.message }); //cannot not complete request
+    }
 }
